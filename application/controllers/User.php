ddfa3802b42280cd988e5a6d325a1782876292bd
thesis-cpe <witthayaniphon@hotmail.com>
2016-02-03 13:56:24 +0700

    login

---
+++ application/controllers/User.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of User
+ *
+ * @author Veriton
+ */
+class User extends CI_Controller {
+    
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    public function index(){
+        
+    }
+    
+    public function login(){
+        $this->load->view('login_view');
+    }
+}
