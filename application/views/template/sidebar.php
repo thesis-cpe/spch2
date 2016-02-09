d55c7d0486eca8a88ed3c65ba40c75de545d3a0f
thesis-cpe <witthayaniphon@hotmail.com>
2016-02-09 14:47:33 +0700

    เมนูซ้าย

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -1,4 +1,4 @@
-	<div class="page-sidebar-wrapper ">
+	<div class="page-sidebar-wrapper">
 		<!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
 		<!-- DOC: Change data-auto-speed="200" to adjust the sub menu slide up/down speed -->
 		<div class="page-sidebar md-shadow-z-2-i  navbar-collapse collapse">
@@ -13,12 +13,116 @@
 				<li class="start ">
 					<a href="index.html">
 					<i class="icon-home"></i>
-					<span class="title">Dashboard</span>
+					<span class="title">หน้าแรก</span>
 					</a>
 				</li>
 			<!--ใส่เมนูเพิ่มเติมตรงนี้-->
-				
-				
+                                <li>  <!--ตั้งค่าระบบ-->
+                                    <a href="javascript:;">
+                                        <i class="icon-settings"></i>
+                                        <span class="title">ตั้งค่าระบบ</span>
+                                        <span class="arrow"></span>
+                                    </a>
+                                    <ul class="sub-menu">
+                                        <li>
+                                            <a href="#">
+                                                <i class="icon-link"></i>
+                                                บริหารสิทธิ์
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <i class="icon-link"></i>
+                                                บริหารสาขา
+                                            </a>
+                                        </li>
+                                         <li>
+                                            <a href="#">
+                                                <i class="icon-link"></i>
+                                                บริหารพนักงาน
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <i class="icon-link"></i>
+                                                บริหารข้อมูล
+                                            </a>
+                                        </li>
+                                    </ul>
+                                    
+                                </li><!--.ตั้งค่าระบบ-->
+                                <!--บันทึกข้อมูล-->
+                                <li>
+					<a href="index.html">
+					<i class="icon-docs"></i>
+					<span class="title">นำเข้าข้อมูล</span>
+					</a>
+				</li>
+                                <!--บันทึกข้อมูล-->
+				<!--ผล-->
+                                <li>  <!--ตั้งค่าระบบ-->
+                                    <a href="javascript:;">
+                                        <i class="icon-bar-chart"></i>
+                                        <span class="title">รายงาน</span>
+                                        <span class="arrow"></span>
+                                    </a>
+                                    <ul class="sub-menu">
+                                        <li>
+                                            <a href="#">
+                                                <i class="icon-link"></i>
+                                                รายวัน
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <i class="icon-link"></i>
+                                                รายเดือน
+                                            </a>
+                                        </li>
+                                         <li>
+                                            <a href="#">
+                                                <i class="icon-link"></i>
+                                                รายปี
+                                            </a>
+                                        </li>
+                                        
+                                    </ul>
+                                    
+                                </li>
+                                <!--.ผล-->
+                                
+                                <!--ช่วยเหลือ-->
+                                <li>  <!--ตั้งค่าระบบ-->
+                                    <a href="javascript:;">
+                                        <i class="icon-support"></i>
+                                        <span class="title">ช่วยเหลือ</span>
+                                        <span class="arrow"></span>
+                                    </a>
+                                    <ul class="sub-menu">
+                                        <li>
+                                            <a href="#">
+                                                <i class="icon-link"></i>
+                                                คู่มือ
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <i class="icon-link"></i>
+                                                นำเข้าฐานข้อมูล
+                                            </a>
+                                        </li>
+                                         <li>
+                                            <a href="#">
+                                                <i class="icon-link"></i>
+                                                ส่งออกฐานข้อมูล
+                                            </a>
+                                        </li>
+                                        
+                                    </ul>
+                                    
+                                </li>
+                                    
+                                <!--.ช่วยเหลือ-->
 				
 			
 			
