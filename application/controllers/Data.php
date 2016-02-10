0b813fb909bd5c421c9b71ba60779a15f18180cc
thesis-cpe <witthayaniphon@hotmail.com>
2016-02-10 14:06:38 +0700

    โครงสร้าง

---
+++ application/controllers/Data.php
@@ -0,0 +1,31 @@
+<?php
+
+class Data extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
+        
+    }
+
+    /* นำเข้า */
+
+    public function add() {
+        
+    }
+
+    /* แก้ไข */
+
+    public function edit() {
+        
+    }
+
+    /* ลบ */
+
+    public function delete() {
+        
+    }
+
+}
---
+++ application/controllers/Manage.php
@@ -0,0 +1,28 @@
+<?php
+
+class Manage extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
+        
+    }
+
+    public function add_category() { // เพิ่มหมวด
+    }
+
+    public function edit_category() { // แก้ไขหมวด
+    }
+
+    public function delete_category() { // ลบหมวด
+    }
+    
+    public function edit_role(){  //ค่าปกติจะให้เห็นหมดแล้วติ๊กออกเอา อาจารย์ตี้
+        
+    }
+    
+    
+
+}
