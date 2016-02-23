5ef1b6305082215495668905c5345f94af6d804f
thesis-cpe <witthayaniphon@hotmail.com>
2016-02-23 15:01:00 +0700

    เปลี่ยนชื่อ

--- template/footer.php
+++ template/footer.php
@@ -1,6 +1,6 @@
 <div class="page-footer">
 	<div class="page-footer-inner">
-		 2016 &copy; System Name by KAN Account. 
+		 2016 &copy; Six Force by KAN Account. 
 	</div>
 	<div class="scroll-to-top">
 		<i class="icon-arrow-up"></i>
