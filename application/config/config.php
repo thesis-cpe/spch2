11546a905a074508cb15565998916b7d53dd6b88
thesis-cpe <witthayaniphon@hotmail.com>
2016-02-27 22:30:51 +0700

    https

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://bi.kanfin.com/';
+$config['base_url'] = 'https://bi.kanfin.com/';
 
 /*
 |--------------------------------------------------------------------------
